923cc3d43efe151687fbd7f0bfc96f54e26fe59a
mattiatoffanin <mattia.toffanin.1@studenti.unipd.it>
2023-12-06 17:13:16 +0100

    ADD calculateAnalytics

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from models.Paris import Paris
 from models.London import London
 from models.SaoPaulo import SaoPaulo
+from utils.CalculateAnalytics import calculateAnalytics
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 def main():
@@ -11,6 +14,17 @@ def main():
     print(paris.graph)
     print(london.graph)
     print(saopaulo.graph)
+    nx.draw(paris.graph, with_labels=True)
+    plt.show()
+    nx.draw(london.graph, with_labels=True)
+    plt.show()
+    nx.draw(saopaulo.graph, with_labels=True)
+    plt.show()
+
+    calculateAnalytics(paris.graph)
+    calculateAnalytics(london.graph)
+    calculateAnalytics(saopaulo.graph)
+
 
 
 if __name__ == "__main__":
---
+++ utils/CalculateAnalytics.py
@@ -0,0 +1,17 @@
+import networkx as nx
+
+
+def calculateAnalytics(graph):
+    # node analytics
+    print(nx.closeness_centrality(graph))
+    print(nx.betweenness_centrality(graph))
+    print(nx.clustering(graph))
+    # graph analytics
+    print(sum(dict(graph.degree()).values()) / len(dict(graph.degree())))
+    if (nx.is_strongly_connected(graph)):
+        print(nx.diameter(graph))
+        print(nx.average_clustering(graph))
+        print(nx.average_shortest_path_length(graph))
+    else:
+        print("graph is not strongly connected")
+    print("________")
